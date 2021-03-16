742b2b7165a53e949000d08dce2a2f3e983eaec4
Fonell <sebfon98@gmail.com>
2021-03-16 22:26:06 +0100

    Fix for adding text (again)

--- NoteAppDAT/src/main/java/com/mycompany/noteappdat/view/NoteBackingBean.java
+++ NoteAppDAT/src/main/java/com/mycompany/noteappdat/view/NoteBackingBean.java
@@ -37,8 +37,14 @@ public class NoteBackingBean implements Serializable {
     }
 
     public void createNote() {
-        fss.createNote(noteName);
+        fss.createNote(noteName, noteText);
         //noteDAO.create(new Note(noteName));
+    }
+
+    public void setNoteText(Note note, String newNoteText) {
+        //FacesMessage message = new FacesMessage();
+        note.setText(newNoteText);
+        noteDAO.update(note);
     }
 
     public void deleteNote() {
@@ -54,7 +60,7 @@ public class NoteBackingBean implements Serializable {
 
     public void createNoteInFolder(String noteName, String folderName) {
         fss.createFolder(folderName);
-        fss.createNote(noteName);
+        //fss.createNote(noteName);
         fss.setNoteParentFolder(fss.getNoteById(0), fss.getFolderById(0));
         //noteDAO.createNote(noteName);
         //noteDAO.setNoteFolder(noteName, folderName);
