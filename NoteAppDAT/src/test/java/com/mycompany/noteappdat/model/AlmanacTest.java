227d405d6e679fa36cc5446c777d982e507a0182
961028 <emilcarlholmsten@gmail.com>
2021-08-23 11:08:32 +0200

    Fixed alamanac test

--- NoteAppDAT/src/test/java/com/mycompany/noteappdat/model/AlmanacTest.java
+++ NoteAppDAT/src/test/java/com/mycompany/noteappdat/model/AlmanacTest.java
@@ -2,19 +2,23 @@ package com.mycompany.noteappdat.model;
 
 import com.mycompany.noteappdat.model.entity.Event;
 import com.mycompany.noteappdat.model.service.Almanac.Almanac;
+import org.jboss.arquillian.junit.Arquillian;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 public class AlmanacTest {
 
-    Almanac almanac = new Almanac();
-
     @Test
     public void insert() {
-        Event event1 = new Event("2001, 1, 0, 0, 0", new GregorianCalendar(2001, 0, 0, 0, 1));
-        Event event2 = new Event("2001, 2, 0, 0, 0", new GregorianCalendar(2001, 1, 0, 0, 1));
+
+        Almanac almanac = new Almanac();
+
+        Event event1 = new Event("2001, 0, 0, 0, 0", new GregorianCalendar(2001, Calendar.JANUARY, 1, 0, 0));
+        Event event2 = new Event("2001, 1, 0, 0, 0", new GregorianCalendar(2001, Calendar.FEBRUARY, 1, 0, 0));
 
         almanac.insert(event1);
         almanac.insert(event2);
@@ -27,11 +31,10 @@ public class AlmanacTest {
         Assert.assertFalse(almanac.getDay(2001, 1, 0).getEvents().contains(event1));
          */
 
-        Assert.assertTrue(almanac.getYear(2001).getMonth(0).getDay(0).getEvents().contains(event1));
-        Assert.assertFalse(almanac.getYear(2001).getMonth(0).getDay(0).getEvents().contains(event2));
+        Assert.assertTrue(almanac.getYear(2001).getMonth(Calendar.JANUARY).getDay(0).getEvents().contains(event1));
+        Assert.assertFalse(almanac.getYear(2001).getMonth(Calendar.JANUARY).getDay(0).getEvents().contains(event2));
 
-        Assert.assertTrue(almanac.getYear(2001).getMonth(1).getDay(0).getEvents().contains(event2));
-        Assert.assertFalse(almanac.getYear(2001).getMonth(1).getDay(0).getEvents().contains(event1));
-
+        Assert.assertTrue(almanac.getYear(2001).getMonth(Calendar.FEBRUARY).getDay(0).getEvents().contains(event2));
+        Assert.assertFalse(almanac.getYear(2001).getMonth(Calendar.FEBRUARY).getDay(0).getEvents().contains(event1));
     }
 }
