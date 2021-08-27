fa36f931a7942876db4b2960afc7ff93a3346e46
961028 <emilcarlholmsten@gmail.com>
2021-08-27 11:34:49 +0200

    html and css updates as well as some code cleanup

--- NoteAppDAT/src/main/java/com/mycompany/noteappdat/view/CalendarBean.java
+++ NoteAppDAT/src/main/java/com/mycompany/noteappdat/view/CalendarBean.java
@@ -12,7 +12,6 @@ import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
-import java.time.LocalDate;
 import java.util.*;
 
 @Data
@@ -23,36 +22,16 @@ public class CalendarBean implements Serializable {
     @Inject
     private CalendarService cs;
 
-    private Event selectedEvent;
-
     private String eventName;
     private String eventText;
-
     private Date date = new Date();
 
     public void update(Event event) {
-        cs.setName(event);
-        cs.setText(event);
-    }
-
-    public void test(ValueChangeEvent e) {
-        eventName = e.getNewValue().toString();
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
+        cs.updateEvent(event);
     }
 
     public void createEvent() {
-        selectedEvent = cs.createEvent(eventName, eventText, date);
-    }
-
-    public void deleteEvent() {
-        cs.removeEvent(selectedEvent);
+        cs.createEvent(eventName, eventText, date);
     }
 
     public void deleteEvent(Event event) {
